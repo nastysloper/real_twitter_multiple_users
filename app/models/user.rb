ec2b49e6f7c59b170c0012a12ca006c3ffa265c7
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-09 16:48:13 -0500

    adding users to model in order to tweet from the app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
   validates :twitter_id, :uniqueness => true
+
+  def twitter_client
+    @twitter_client ||= Twitter::Client.new(
+        :oauth_token => self.token,
+        :oauth_secret => self.secret
+      )
+  end
 end
